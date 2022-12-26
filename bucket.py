f052188fc668a17ea33063725f492c5420059f4b
Danny Van Stemp <danwardvs@adsgames.net>
2022-12-26 12:26:38 -0500

    upload

---
+++ bucket.py
@@ -0,0 +1,42 @@
+import boto3
+import os
+from dotenv import load_dotenv
+import datetime
+
+
+load_dotenv()
+
+ACCESS_KEY = os.getenv('ACCESS_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
+CLUSTER_URL = os.getenv('CLUSTER_URL')
+BUCKET_LABEL = os.getenv('BUCKET_LABEL')
+
+linode_obj_config = {
+    "aws_access_key_id": ACCESS_KEY,
+    "aws_secret_access_key": SECRET_KEY,
+    "endpoint_url":CLUSTER_URL,
+}
+
+client = boto3.client("s3", **linode_obj_config)
+
+
+ct = datetime.datetime.now()
+
+# ts store timestamp of current time
+ts = ct.timestamp()
+stamp = round(ts)
+filename = "daily_" + str(stamp) + ".txt"
+print(filename)
+f = open("data/" + filename, "x")
+f.write("this is the data")
+f.close()
+
+# print(client.list_objects(
+#     Bucket=BUCKET_LABEL,)["Contents"])
+
+# client.put_object()
+
+client.upload_file(
+    Filename='data/' +filename,
+    Bucket=BUCKET_LABEL,
+    Key=filename)
